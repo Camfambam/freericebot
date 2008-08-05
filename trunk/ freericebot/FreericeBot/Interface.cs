a16d6743d02640e8cbfe9d0a662eed1fe1d81e89
stiney405 <stiney405@cdf55b7e-b653-0410-ad6a-6955ef3d12d6>
2008-08-05 19:05:43 +0000

    A lot of changes: AddedWebRequestWrapper.cs and code for it implemented all the functions in Freerice Site Interaction.cs - not tested very well

---
+++ trunk/ freericebot/FreericeBot/Interface.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FreericeBot
+{
+    public partial class Interface : Form
+    {
+        public Interface()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 2; i++ )
+                {
+                Console.AppendText(Freerice_Site_Interaction.GetLookupWord() + "\r\n\t");
+
+                foreach (string s in Freerice_Site_Interaction.GetPossibleAnswers())
+                    {
+                    Console.AppendText("\"" + s + "\" ");
+                    }
+                Console.AppendText("\r\n");
+                Console.AppendText("\r\n");
+                Console.AppendText("\r\n");
+
+                Random r = new Random();
+                Freerice_Site_Interaction.SubmitAnswer(r.Next(0,3));
+
+                Console.AppendText(
+                    Freerice_Site_Interaction.IsAnswerCorrect() ? "Correct answer" : "Incorrect answer");
+
+                Console.AppendText("\r\n");
+
+                Console.AppendText(Freerice_Site_Interaction.GetCorrectWordSynonym() + "\r\n");
+
+                Console.AppendText("Rice donated: " + Freerice_Site_Interaction.GetRiceDonated() + "\r\n");
+                //Freerice_Site_Interaction.ReloadSite();
+                }
+            
+        }
+    }
+}
---
+++ trunk/ freericebot/FreericeBot/WebRequestWrapper.cs
@@ -0,0 +1,76 @@
+﻿///
+/// Author: Aron J
+/// Date: 08/05/08
+
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace FreericeBot
+{
+    class StatusNotOK : Exception
+        {
+        public StatusNotOK()
+            {
+            }
+        public StatusNotOK(string message) : base(message)
+            {
+            }
+        public StatusNotOK(string message, Exception inner) : base(message, inner)
+            {
+            }
+
+        }
+
+    class WebRequestWrapper
+        {
+        private enum StatusCodes
+            {
+            OK = 200
+            }
+
+        public string GetStringRepresentationOfSite(string site)
+            {
+            WebRequest request = WebRequest.Create(site);
+            return GetStringRepresentationOfSite(request);
+            }
+
+        public string GetStringRepresentationOfSite(string site, string method, string data)
+            {
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            Byte[] byteData = encoding.GetBytes(data);
+
+            WebRequest request = WebRequest.Create(site);
+            request.Method = method;
+            request.ContentLength = byteData.Length;
+            request.ContentType = "application/x-www-form-urlencoded";
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteData, 0, byteData.Length);
+            dataStream.Close();
+
+            return GetStringRepresentationOfSite(request);
+            }
+
+        private static string GetStringRepresentationOfSite(WebRequest request)
+            {
+            
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            
+            if((int)response.StatusCode != (int)StatusCodes.OK)
+                {
+                throw new StatusNotOK("Code returned " + response.StatusCode);
+                }
+
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            return responseFromServer;
+            }
+       }
+}
