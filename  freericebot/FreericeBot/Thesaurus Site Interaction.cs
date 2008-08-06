0539d584aa62737751fc7b6c962ec4e71acfe688
stiney405 <stiney405@cdf55b7e-b653-0410-ad6a-6955ef3d12d6>
2008-08-06 01:41:48 +0000

    Added code for getting Thesaurus information

---
+++  freericebot/FreericeBot/Thesaurus Site Interaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace FreericeBot
+    {
+    internal static class Thesaurus_Site_Interaction
+        {
+        private const string siteUrl = "http://thesaurus.publicdomaindb.org/{0}/{1}.html";
+        private static readonly WebRequestWrapper webWrapper = new WebRequestWrapper();
+
+        public static string[] GetSynonymsOfWord(string word)
+            {
+            string url = string.Format(siteUrl, word.ToUpper()[0], word.ToLower());
+            string siteData = webWrapper.GetStringRepresentationOfSite(url);
+            Regex rx = new Regex(@"<div\ class=""entry"">.+<b>.*</b>:<i>\ (?<words>.*)</i><br\ />", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            return rx.Match(siteData).Result("${words}").Split(new Char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
