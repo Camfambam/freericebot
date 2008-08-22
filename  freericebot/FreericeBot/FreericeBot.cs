8e631a3ed0fb791c7a9be5a7fdfb80a642549bc5
stiney405 <stiney405@cdf55b7e-b653-0410-ad6a-6955ef3d12d6>
2008-08-22 02:08:45 +0000

    Started coding the main bot loop that will run in a thread.

---
+++  freericebot/FreericeBot/FreericeBot.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace FreericeBot
+{
+    class FreericeBot
+    {
+        public delegate void StatusUpdateHandler(string message);
+        public event StatusUpdateHandler statusUpdate;
+
+        private readonly Thread botLoopThread;
+
+        FreericeBot()
+            {
+            botLoopThread = new Thread(FreericeBot.BotLoop);
+            }
+
+        public void Start()
+            {
+            botLoopThread.Start();
+            }
+
+        public void Stop()
+            {
+            botLoopThread.Abort();
+            }
+
+        private static void BotLoop()
+            {
+            Freerice_Site_Interaction freeRiceInteraction = new Freerice_Site_Interaction();
+
+            while(true)
+                {
+                FreericeSiteData data = new FreericeSiteData();
+                try
+                    {
+                     data = freeRiceInteraction.GetSiteData();
+                    }
+                catch(ErrorLoadingFreerice s)
+                    {
+                    continue;
+                    }
+
+                FreericeAnswerData answerData = new FreericeAnswerData();
+                try
+                    {
+                    answerData = freeRiceInteraction.SubmitAnswer(data, 1);
+                    }
+                catch(ErrorLoadingFreerice s)
+                    {
+                    
+                    }
+                }  
+            }
+    }
+}
